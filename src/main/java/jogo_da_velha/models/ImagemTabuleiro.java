b68404ff1bc5300dfe0247196bb1eafaaf487b62
Edilson Alexandre Cuamba <edilsoncuamba@gmail.com>
2021-11-06 01:40:45 +0200

    A classe ImagemTabuleiro não pode ser abstrata porque é instanciada dentro da classe Tabuleiro e a classe tabuleiro não conhece as classes concretas que criamos.

--- src/main/java/jogo_da_velha/models/ImagemTabuleiro.java
+++ src/main/java/jogo_da_velha/models/ImagemTabuleiro.java
@@ -4,10 +4,10 @@ package jogo_da_velha.models;
  * Nov 5, 2020
  * Edilson A. Cuamba
  * Atualizado no dia 5 de Novembro de 2021 por Edilson Alexandre Cuamba
- * Está classe abstrata representa uma abstração da(o) imagem/estado actual do tabuleiro com mensagens, array do tabuleiro actual, Jogador actual, Jogador Vencedor, se houve vencedor (boolean).
+ * Está classe representa uma imagem/estado actual do tabuleiro com mensagens, array do tabuleiro actual, Jogador actual, Jogador Vencedor, se houve vencedor (boolean).
  */
 
-public abstract class ImagemTabuleiro {
+public class ImagemTabuleiro {
     private String mensagem;
     private Posicao[][] posicoes;
     private Jogador jogadorActual;
