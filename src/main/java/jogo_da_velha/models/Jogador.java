5ef668da7292802060c4511b1971173b2082d52f
Edilson Alexandre Cuamba <edilsoncuamba@gmail.com>
2021-11-05 23:39:57 +0200

    Adicionando documentação ao código sobre a classe abstrata Jogador.
    OAdicionei um construtor com parâmetro que espera um nome (String) como argumento.

--- src/main/java/jogo_da_velha/models/Jogador.java
+++ src/main/java/jogo_da_velha/models/Jogador.java
@@ -1,46 +1,57 @@
 package jogo_da_velha.models;
 
-/*
-*Nov 5, 2020
-*Edilson A. Cuamba
-*
-*/
+/**
+ * Nov 5, 2020
+ * Edilson A. Cuamba
+ * <p>
+ * Atualizado no dia 5 de Novembro de 2021 por Edilson Alexandre Cuamba
+ * Esta classe abstrata é uma representa abstrata de um jogador, com todos os dados cruciais para o jogo poder decorrer.
+ */
 
 public abstract class Jogador {
-	private String nome;
-	private char simbolo;
-	private boolean minhaVez;
-	private boolean vencedor;
-	
-	public void setNome(String nome) {
-		this.nome = nome;
-	}
-	public String getNome() {
-		return this.nome;
-	}
-	
-	public void setSimbolo(char simbolo) {
-		this.simbolo = simbolo;
-	}
-	public char getSimbolo() {
-		return this.simbolo;
-	}
-	
-	public void setMinhaVez(boolean minhaVez) {
-		this.minhaVez = minhaVez;
-	}
-	public boolean isMinhaVez() {
-		return this.minhaVez;
-	}
-	
-	public void setVencedor(boolean vencedor) {
-		this.vencedor = vencedor;
-	}
-	public boolean isVencedor() {
-		return this.vencedor;
-	}
-	
-	
-	
-	
+    private String nome;
+    private Character simbolo;
+    private boolean minhaVez;
+    private boolean souVencedor;
+
+    public Jogador() {
+    }
+
+    public Jogador(String nome) {
+        this.nome = nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getNome() {
+        return this.nome;
+    }
+
+    public void setSimbolo(Character simbolo) {
+        this.simbolo = simbolo;
+    }
+
+    public Character getSimbolo() {
+        return this.simbolo;
+    }
+
+    public void setMinhaVez(boolean minhaVez) {
+        this.minhaVez = minhaVez;
+    }
+
+    public boolean isMinhaVez() {
+        return this.minhaVez;
+    }
+
+    public void setSouVencedor(boolean souVencedor) {
+        this.souVencedor = souVencedor;
+    }
+
+    public boolean isSouVencedor() {
+        return this.souVencedor;
+    }
+
+
 }
