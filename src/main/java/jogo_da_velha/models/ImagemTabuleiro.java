5089add5580857d46b025c64b4d17364a42f8ac1
Edilson Alexandre Cuamba <edilsoncuamba@gmail.com>
2021-11-06 01:09:55 +0200

    Adicionei mais campos para a ImagemTabuleiro ter mais informações úteis para view, para que qualquer view consiga usar.
    Campos novos:
    Jogador jogadorActual;
    Jogaodor jogadorVencedor;
    boolean houveVencedor;

--- src/main/java/jogo_da_velha/models/ImagemTabuleiro.java
+++ src/main/java/jogo_da_velha/models/ImagemTabuleiro.java
@@ -4,27 +4,41 @@ package jogo_da_velha.models;
  * Nov 5, 2020
  * Edilson A. Cuamba
  * Atualizado no dia 5 de Novembro de 2021 por Edilson Alexandre Cuamba
- * Está classe abstrata representa uma abstração da(o) imagem/estado actual do tabuleiro com mensagens e o array do tabuleiro actual.
+ * Está classe abstrata representa uma abstração da(o) imagem/estado actual do tabuleiro com mensagens, array do tabuleiro actual, Jogador actual, Jogador Vencedor, se houve vencedor (boolean).
  */
 
 public abstract class ImagemTabuleiro {
     private String mensagem;
     private Posicao[][] posicoes;
+    private Jogador jogadorActual;
+    private Jogador jogadorVencedor;
+    private boolean houveVencedor;
 
-    public void setMensagem(String mensagem) {
+    public ImagemTabuleiro(String mensagem, Posicao[][] posicoes, Jogador jogadorActual, Jogador jogadorVencedor, boolean houveVencedor) {
         this.mensagem = mensagem;
+        this.jogadorActual = jogadorActual;
+        this.jogadorVencedor = jogadorVencedor;
+        this.posicoes = posicoes;
+        this.houveVencedor = houveVencedor;
     }
 
     public String getMensagem() {
         return this.mensagem;
     }
 
-
-    public void setPosicoes(Posicao[][] posicoes) {
-        this.posicoes = posicoes;
-    }
-
     public Posicao[][] getPosicoes() {
         return this.posicoes;
+    }
+
+    public Jogador getJogadorActual() {
+        return jogadorActual;
+    }
+
+    public Jogador getJogadorVencedor() {
+        return jogadorVencedor;
+    }
+
+    public boolean isHouveVencedor() {
+        return houveVencedor;
     }
 }
