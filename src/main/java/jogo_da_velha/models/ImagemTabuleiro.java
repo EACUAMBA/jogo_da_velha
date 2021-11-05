fd4490e0d28fd9b854577324c4bddb42ae846e39
Edilson Alexandre Cuamba <edilsoncuamba@gmail.com>
2021-11-06 00:00:29 +0200

    Adicionando documentação ao código sobre a classe abstrata ImagemTabuleiro.
    Tornei a classe abstrata para poder herdar e adicionar mais informações sobre ela.

--- src/main/java/jogo_da_velha/models/ImagemTabuleiro.java
+++ src/main/java/jogo_da_velha/models/ImagemTabuleiro.java
@@ -1,28 +1,30 @@
 package jogo_da_velha.models;
 
-/*
-*Nov 5, 2020
-*Edilson A. Cuamba
-*
-*/
+/**
+ * Nov 5, 2020
+ * Edilson A. Cuamba
+ * Atualizado no dia 5 de Novembro de 2021 por Edilson Alexandre Cuamba
+ * Está classe abstrata representa uma abstração da(o) imagem/estado actual do tabuleiro com mensagens e o array do tabuleiro actual.
+ */
 
-public class ImagemTabuleiro {
-	private String mensagem;
-	private Posicao posicoes[][];
-	
-	
-	public void setMensagem(String mensagem) {
-		this.mensagem = mensagem;
-	}
-	public String getMensagem() {
-		return this.mensagem;
-	}
-	
-	
-	public void setPosicoes(Posicao[][] posicoes) {
-		this.posicoes = posicoes;
-	}
-	public Posicao[][] getPosicoes() {
-		return this.posicoes;
-	}
+public abstract class ImagemTabuleiro {
+    private String mensagem;
+    private Posicao[][] posicoes;
+
+    public void setMensagem(String mensagem) {
+        this.mensagem = mensagem;
+    }
+
+    public String getMensagem() {
+        return this.mensagem;
+    }
+
+
+    public void setPosicoes(Posicao[][] posicoes) {
+        this.posicoes = posicoes;
+    }
+
+    public Posicao[][] getPosicoes() {
+        return this.posicoes;
+    }
 }
