51a870f5b800e940423d1f37243b9fbf9d729fdd
Edilson Alexandre Cuamba <edilsoncuamba@gmail.com>
2021-11-05 23:32:33 +0200

    Adicionando documentação ao código sobre a classe Posição.

--- src/main/java/jogo_da_velha/models/Posicao.java
+++ src/main/java/jogo_da_velha/models/Posicao.java
@@ -1,27 +1,36 @@
 package jogo_da_velha.models;
 
-/*
-*Nov 5, 2020
-*Edilson A. Cuamba
-*
-*/
-
+/**
+ * Nov 5, 2020
+ * Edilson A. Cuamba
+ *
+ * Atualizado no dia 5 de Novembro de 2021
+ * Classe responsável por <b>representar uma posição no tabuleiro</b>, onde uma posição pode esta ocupada se tiver uma instância de Jogador ou não caso seja nula.
+ */
 public class Posicao {
-	private Jogador jogador;
-	
-	public Posicao() {}
-	public Posicao(Jogador jogador) {this.jogador = jogador;}
-	
-	public void setJogador(Jogador jogador) {
-		this.jogador = jogador;
-	}
-	public Jogador getJogador() {
-		return this.jogador;
-	}
+    private Jogador jogador;
+
+    public Posicao() {
+    }
+
+    public Posicao(Jogador jogador) {
+        this.jogador = jogador;
+    }
+
+    public void setJogador(Jogador jogador) {
+        this.jogador = jogador;
+    }
+
+    public Jogador getJogador() {
+        return this.jogador;
+    }
+
+	/**
+	 * Verifica se a posição actual está ocupada, retorna <b>true</b> se estiver ocupada.
+	 *
+	 * @return boolean
+	 */
 	public boolean posicaoEstaOcupada() {
-		if(this.jogador != null) {
-			return true;
-		}
-		return false;
-	}
+    	return this.jogador != null;
+    }
 }
